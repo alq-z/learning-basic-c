67b11f72973d1b078ae4e42f00f94454b5f65647
AL QUINIÑEZA <alquinineza14@gmail.com>
2026-06-06 23:36:26 +0800

    Change name to 'Johnny' and add age and weight
    Updated the name variable and added age and weight variables.

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    char name[] = "Jhon";
+    char name[] = "Johnny";
+    int age = 35;
+    float weight = 45.5;
     
     // comment
     printf("I like cokies\n");
